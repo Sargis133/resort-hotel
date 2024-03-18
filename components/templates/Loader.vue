263008612314023affe875a9d524ca4dd9a206bf
Sargis <saqobabloyan@gmail.com>
2024-03-18 12:59:12 +0400

    add loader

---
+++ components/templates/Loader.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="loader-content">
+  <div class="loader-content__loader-box">
+    <div class="loader-box__loader">
+      <div class="loader-rotate"></div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+.loader-content {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 100;
+  background-color: rgba(0, 0, 0, 0.9);
+}
+.loader-content__loader-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+.loader-box__loader {
+  width: 100px;
+  height: 100px;
+  background-color: transparent;
+  border-radius: 50%;
+  position: relative;
+}
+.loader-rotate {
+  position: absolute;
+  border-left: 5px solid gray;
+  border-radius: 50%;
+  transform: rotate(0deg);
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  animation: rotate infinite;
+  animation-duration: 2s;
+  animation-timing-function: linear;
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,112 @@
 <template>
-  <div class="main">
-    <div class="content">
-      <div class="resort-content">
-        <div class="resort-content__title">
-          <h3 class="titles-style hide" id="firstTitle">Welcome To Palma Resort</h3>
-        </div>
-      </div>
-      <div class="about-content">
-        <div class="about-content-bg-image"></div>
-        <div class="about-content__about-description">
-          <div class="about-description__title-box">
-            <h3 class="titles-style hide" id="aboutTitle">About Us</h3>
+    <div class="main">
+      <div class="content">
+        <div class="resort-content">
+          <div class="resort-content__title">
+            <h3 class="titles-style hide" id="firstTitle">
+              Welcome To Palma Resort
+            </h3>
           </div>
-          <div class="about-description__description-box">
-            <div id="lPalm" class="about-palm-left">
+        </div>
+        <div class="about-content">
+          <div class="about-content-bg-image"></div>
+          <div class="about-content__about-description">
+            <div class="about-description__title-box">
+              <h3 class="titles-style hide" id="aboutTitle">About Us</h3>
             </div>
-            <p>Welcome to Palma Resort - an oasis of luxury and comfort, where every moment turns into an unforgettable experience. We offer the perfect combination of first-class service, exquisite amenities and breathtaking nature to make your holiday unique.
-            </p>
-            <p>Our resort is located in a picturesque corner of nature, away from the noise of the city bustle. Palma Resort is a place where luxury meets comfort and service exceeds expectations. Our rooms are equipped with everything you need for a pleasant and carefree stay, providing you with the highest level of satisfaction.
-            </p>
-            <p>Our team is made up of professionals dedicated to creating an environment of exceptional service. We strive to provide Palma Resort guests with a unique experience that will remain in their memories for years to come. We are proud that we can please even the most sophisticated tastes.
-            </p>
-            <p>
-              Palma Resort is not just a place to relax, it is a world of comfort and luxury, where every moment becomes a memory of an ideal vacation. We invite you to an unforgettable journey into the world of true pleasure and relaxation.
-            </p>
-            <div id="rPalm" class="about-palm-right"></div>
-          </div>
-        </div>
-      </div>
-      <div class="rooms-content">
-        <div class="rooms-content__bg-image"></div>
-        <div class="rooms-content__title">
-          <h3 id="roomTitle" class="titles-style">Choose Your Room</h3>
-        </div>
-        <div class="rooms-content__rooms-aside">
-          <div class="rooms-aside__rooms-slide">
-            <Slider/>
-          </div>
-          <div class="rooms-aside__rooms-book">
-            <div class="rooms-book__title-box">
-              <h3 class="titles-style">Book Your Room</h3>
+            <div class="about-description__description-box">
+              <div id="lPalm" class="about-palm-left"></div>
+              <p>
+                Welcome to Palma Resort - an oasis of luxury and comfort, where
+                every moment turns into an unforgettable experience. We offer
+                the perfect combination of first-class service, exquisite
+                amenities and breathtaking nature to make your holiday unique.
+              </p>
+              <p>
+                Our resort is located in a picturesque corner of nature, away
+                from the noise of the city bustle. Palma Resort is a place where
+                luxury meets comfort and service exceeds expectations. Our rooms
+                are equipped with everything you need for a pleasant and
+                carefree stay, providing you with the highest level of
+                satisfaction.
+              </p>
+              <p>
+                Our team is made up of professionals dedicated to creating an
+                environment of exceptional service. We strive to provide Palma
+                Resort guests with a unique experience that will remain in their
+                memories for years to come. We are proud that we can please even
+                the most sophisticated tastes.
+              </p>
+              <p>
+                Palma Resort is not just a place to relax, it is a world of
+                comfort and luxury, where every moment becomes a memory of an
+                ideal vacation. We invite you to an unforgettable journey into
+                the world of true pleasure and relaxation.
+              </p>
+              <div id="rPalm" class="about-palm-right"></div>
             </div>
-            <Book/>
+          </div>
+        </div>
+        <div class="rooms-content">
+          <div class="rooms-content__bg-image"></div>
+          <div class="rooms-content__title">
+            <h3 id="roomTitle" class="titles-style">Choose Your Room</h3>
+          </div>
+          <div class="rooms-content__rooms-aside">
+            <div class="rooms-aside__rooms-slide">
+              <Slider />
+            </div>
+            <div class="rooms-aside__rooms-book">
+              <div class="rooms-book__title-box">
+                <h3 class="titles-style">Book Your Room</h3>
+              </div>
+              <Book />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-
+  <template v-if="loader">
+    <Loader />
+  </template>
 </template>
 
 <script setup lang="ts">
-import {onRotatePalmFunc} from "~/components/templates/index/utils/rotatePalm";
-import {onShowTtitleFunc} from "~/components/templates/index/utils/showTitles";
+import { onRotatePalmFunc } from "~/components/templates/index/utils/rotatePalm";
+import { onShowTtitleFunc } from "~/components/templates/index/utils/showTitles";
+import Loader from "~/components/templates/Loader.vue";
 
-onMounted(() => onShowTtitleFunc('firstTitle', 'show'))
+let loader = ref(true);
 
-if(process.client) {
-  window.addEventListener('scroll', function() {
-    let aboutSection = document.getElementById('aboutTitle')
-    let roomTitle = document.getElementById('roomTitle')
-        if(aboutSection && aboutSection.getBoundingClientRect().top - window.scrollY < -100) {
-          onShowTtitleFunc('aboutTitle','show')
-          onRotatePalmFunc('show')
-        } else {
-          onRotatePalmFunc('hide')
-          onShowTtitleFunc('aboutTitle', 'hide')
-        }
-        if(roomTitle && roomTitle.getBoundingClientRect().top - window.scrollY < -300) {
-          onShowTtitleFunc('roomTitle', 'show')
-        } else {
-          onShowTtitleFunc('roomTitle', 'hide')
-        }
-  })
+onMounted(() => {
+  onShowTtitleFunc("firstTitle", "show");
+  loader.value = false;
+});
+
+if (process.client) {
+  window.addEventListener("scroll", function () {
+    let aboutSection = document.getElementById("aboutTitle");
+    let roomTitle = document.getElementById("roomTitle");
+    if (
+      aboutSection &&
+      aboutSection.getBoundingClientRect().top - window.scrollY < -100
+    ) {
+      onShowTtitleFunc("aboutTitle", "show");
+      onRotatePalmFunc("show");
+    } else {
+      onRotatePalmFunc("hide");
+      onShowTtitleFunc("aboutTitle", "hide");
+    }
+    if (
+      roomTitle &&
+      roomTitle.getBoundingClientRect().top - window.scrollY < -300
+    ) {
+      onShowTtitleFunc("roomTitle", "show");
+    } else {
+      onShowTtitleFunc("roomTitle", "hide");
+    }
+  });
 }
-
-
 </script>
 
 <style scoped>
@@ -110,15 +141,9 @@ if(process.client) {
   transition: all 800ms;
 }
 
-
-
 @media screen and (max-width: 1024px) {
   .titles-style {
     font-size: 30px;
   }
 }
-
-
-
-
 </style>
