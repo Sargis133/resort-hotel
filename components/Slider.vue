180c76b6fe96eaaa2bc38006f0013688be9dbe0a
Sargis <saqobabloyan@gmail.com>
2024-04-26 15:23:49 +0400

    change images url

--- components/Slider.vue
+++ components/Slider.vue
@@ -23,11 +23,11 @@
 import Icons from "~/components/Icons.vue";
 
 let sliderImages = ref([
-  "images/rooms/2-room(1).jpg",
-  "images/rooms/2-room(2).jpg",
-  "images/rooms/2-room(3).jpg",
-  "images/rooms/2-room(4).jpg",
-  'images/rooms/2-room(5).jpg'
+  "rooms/2-room(1).jpg",
+  "rooms/2-room(2).jpg",
+  "rooms/2-room(3).jpg",
+  "rooms/2-room(4).jpg",
+  'rooms/2-room(5).jpg'
 ]);
 
 function slideToRightFunc() {
